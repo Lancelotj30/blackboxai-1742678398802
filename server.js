026d90c882805c1d85382251649c094665a37d13
Lancelotj30 <Lancelotj30@gmail.com>
2025-03-22 14:20:47 -0700

    Add server.js 1742678447328

---
+++ server.js
@@ -0,0 +1,50 @@
+{
+  "workbench.colorTheme": "Default Light Modern",
+  "workbench.iconTheme": "vs-seti",
+  "editor.autoClosingBrackets": "languageDefined",
+  "editor.autoClosingQuotes": "languageDefined",
+  "editor.scrollBeyondLastLine": true,
+  "editor.mouseWheelZoom": true,
+  "editor.wordBasedSuggestions": "off",
+  "editor.acceptSuggestionOnEnter": "on",
+  "editor.foldingHighlight": false,
+  "editor.semanticHighlighting.enabled": true,
+  "editor.bracketPairColorization.enabled": false,
+  "editor.fontSize": 12,
+  "accessibility.signals.lineHasError": {
+    "sound": "on"
+  },
+  "accessibility.signals.onDebugBreak": {
+    "sound": "on"
+  },
+  "files.autoSave": "off",
+  "debug.toolBarLocation": "docked",
+  "editor.experimental.asyncTokenization": true,
+  "typescript.tsserver.log": "normal",
+  "workbench.sideBar.location": "left",
+  "editor.experimental.preferTreeSitter": ["typescript"],
+  "workbench.activity.showAccounts": false,
+  "terminal.integrated.shellIntegration.enabled": true,
+  "terminal.integrated.defaultProfile.linux": "bash",
+  "terminal.integrated.profiles.linux": {
+    "bash": {
+      "path": "/bin/bash",
+      "args": ["--login"],
+      "icon": "terminal-bash"
+    }
+  },
+  "terminal.integrated.automationShell.linux": "/bin/bash",
+  "terminal.integrated.shellIntegration.decorationsEnabled": "both",
+  "terminal.integrated.enablePersistentSessions": false,
+  "security.workspace.trust.enabled": true,
+  "blackbox.shellMode": true,
+  "terminal.integrated.env.linux": {
+    "SHELL": "/bin/bash",
+    "TERM": "xterm-256color"
+  },
+  "terminal.integrated.inheritEnv": true,
+  "workbench.startupEditor": "newUntitledFile",
+  "workbench.editor.showTabs": "multiple",
+  "notifications.enabled": false,
+  "workbench.notifications.showInStatusBar": "never"
+}
